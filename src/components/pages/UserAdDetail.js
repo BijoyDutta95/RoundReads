4a5d97a0a5ab5941942c21ce7371dca8364a06ce
Naba Jyoti Gogoi <73441798+nabajtg@users.noreply.github.com>
2020-12-07 15:07:48 +0530

    Create UserAdDetail.js

---
+++ src/components/pages/UserAdDetail.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import AccountInfo from '../AccountInfo'
+import UserAdItems from '../UserAdItems'
+import './GlobalChange.css'
+//hekkoo
+function UserAd() {
+    return (
+        <div className="globalBlock">
+            <AccountInfo/>
+        
+            
+        <UserAdItems/>
+        </div>
+    )
+}
+
+export default UserAd
