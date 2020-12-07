7038df366c06d0dbdd9322bb63b0be6fb0378da1
Naba Jyoti Gogoi <nabajtg@gmail.com>
2020-12-07 10:32:28 +0530

    Current User Ads displayed in MyAds and dlete feature from MyAds added

--- src/components/UserAdItems.js
+++ src/components/UserAdItems.js
@@ -2,22 +2,75 @@ import React from 'react'
 import './UserAdItems.css'
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
+import { UserContext } from './Context/Contexts';
+import Axios from 'axios';
+
 function UserAdItems() {
-    return (
-        <div id="userItemsBlock">
-            <img src="/images/bookImg.jpg" alt="itemImage" id="itemImage"/>
-            <div id="itemInfo">
-                <strong>Status: sold/unsold </strong>
-                <strong>Views: 30 </strong>
-                <strong>Price: 420</strong>
-                
+    const {userSession} = React.useContext(UserContext)
+    const [items, setItems] = React.useState([])
+    
+    React.useEffect(() =>{
+        function getUserItems(){
+            let url = "http://localhost:8000/api/get_user_books/?search=" + JSON.parse(userSession).email
+            console.log("User items called")
+            Axios.get(url)
+            .then(data =>{
+                console.log(data.data)
+                setItems(data.data)
+            })
+            .catch(err =>{
+                console.log(err)
+            })
+        }    
+        getUserItems()
+    }, [])
+
+    const deleteItem = (id) =>{
+        console.log("book to delete : " + id)
+        let itemsTemp = []
+        for(let i in items){
+            if(items[i].id != id){
+                itemsTemp.push(items[i])
+            }
+        }
+        console.log("after delete : " + itemsTemp)
+        setItems(itemsTemp)
+        let url = "http://localhost:8000/api/books/" + id
+        Axios.delete(url)
+        .then(data =>{
+            console.log("success " + data.data)
+        })
+        .catch(err =>{
+            console.log(err)
+        })
+    }
+    
+    
+    const renderItem = (item, index) =>{
+        return (
+            
+            <div id="userItemsBlock" key={index}>
+                <img src={item.image1} alt="itemImage" id="itemImage"/>
+                <div id="itemInfo">
+                    <strong>Title : {item.title} </strong>
+                    <strong>Views: 30 </strong>
+                    <strong>Price: {item.price}</strong>
+                    
+                </div>
+                <div id="itemButton">
+                    <DeleteIcon id="deleteIcon" onClick={() => deleteItem(item.id)}/>
+                    <EditIcon id="editIcon"/>
+                </div>
             </div>
-            <div id="itemButton">
-                <DeleteIcon id="deleteIcon"/>
-                <EditIcon id="editIcon"/>
-            </div>
+        )
+    }
+
+    return (   
+        <div id="itemsDiplayedBlock">
+            {items.map(renderItem)}
         </div>
     )
+
 }
 
 export default UserAdItems
