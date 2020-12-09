83efc28f9030f1e6a901c1b062cc28bd855343b3
Bijoy <bijoy10dutta@gmail.com>
2020-12-10 00:23:47 +0530

    Message Icon added in my ad cards. Message Page yet to be implemented

---
+++ src/components/MessageCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MessageCard() {
+    return (
+        <div>
+            This is message Card
+        </div>
+    )
+}
+
+export default MessageCard
---
+++ src/components/pages/Messages.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MessageCard from '../MessageCard'
+
+function Messages() {
+    return (
+        <div>
+            <MessageCard/>
+        </div>
+    )
+}
+
+export default Messages
