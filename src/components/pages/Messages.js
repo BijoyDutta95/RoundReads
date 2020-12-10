90901eeda9bb0d20e8f9353be6fb3b486f9e6c32
Bijoy <bijoy10dutta@gmail.com>
2020-12-10 20:48:56 +0530

    Messages Page implemented

--- src/components/pages/Messages.js
+++ src/components/pages/Messages.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import MessageCard from '../MessageCard'
-
+import Mailbox from '../../icons/mailbox.svg';
+import './GlobalChange.css'
 function Messages() {
     return (
-        <div>
+        <div className="globalBlock">
+            <div id="noMessages">
+                <p>Empty Mail Box!</p>
+                <p>It seems lonely here! Try Posting a new Ad</p>
+                <img src={Mailbox} alt="mailbox" id="mailImage"/>
+            </div>
+            <MessageCard/>
             <MessageCard/>
         </div>
     )
 }
-
 export default Messages
