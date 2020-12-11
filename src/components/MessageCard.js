eb4e40a3362d523bf9ede5ec06ddf9919979bfc1
Bijoy <bijoy10dutta@gmail.com>
2020-12-11 19:26:22 +0530

    Accept or Decline icons added in Message Card and accept functoinality implemented

--- src/components/MessageCard.js
+++ src/components/MessageCard.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import './MessageCard.css'
+import CheckIcon from '@material-ui/icons/Check';
+import CloseIcon from '@material-ui/icons/Close';
+import ReplyPopUp from './ReplyPopUp';
+
 function MessageCard() {
+    const replyRef=React.useRef();
+
+    const openPopUp=()=>{
+        replyRef.current.openModal();
+    }
+
     return (
         <div id="messageCard">
             <img src="/images/bookImg.jpg"/>
             <div id="messageText">
-                <p>Received messageReceived messageReceived messageReceived messageReceived message................................</p>
+                <p id="receivedMessage">Received messageReceived messageReceived messageReceived messageReceived message................................</p>
                 <hr/>
                 <div id="offers">
                     <label>Offered Amount: </label>
@@ -13,6 +23,12 @@ function MessageCard() {
                     <label>Requested Duration: </label>
                     <p>2months</p>
                 </div>
+            </div>
+            <div id="vl"></div>
+            <div id="messageButtons">
+                <CheckIcon id="accept" onClick={openPopUp}/>
+                <ReplyPopUp ref={replyRef}/>
+                <CloseIcon id="decline"/>
             </div>
         </div>
     )
