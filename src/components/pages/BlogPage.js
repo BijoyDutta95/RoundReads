23722c8661474abb266c0d599fbc48305072a553
Bijoy <bijoy10dutta@gmail.com>
2020-12-21 14:13:48 +0530

    Blog page implementation completed

--- src/components/pages/BlogPage.js
+++ src/components/pages/BlogPage.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import LatestBlogs from '../LatestBlogs'
+import TrendingBlogs from '../TrendingBlogs'
+import WriteBlog from '../WriteBlog'
 import IndividualItem from './IndividualItem'
 
 function BlogPage() {
     return (
         <div className="globalBlock">
             <LatestBlogs/>
+            <TrendingBlogs/>
+            <WriteBlog/>
         </div>
     )
 }
