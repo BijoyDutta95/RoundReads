290e10d06077a11930b917df2d29b7526b2e1a67
Naba Jyoti Gogoi <nabajtg@gmail.com>
2020-12-25 00:05:49 +0530

    Similar Items Displayed in Indivitual Items

--- src/components/SimilarBooks.js
+++ src/components/SimilarBooks.js
@@ -1,68 +1,120 @@
 import React from 'react'
 import './SimilarBooks.css'
-
-function SimilarBooks() {
-    return (
-        <div id="similarBooksBlock">
-            <h3>Similar Items</h3>
-            <div id="similarCards">
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
-                <p>lkajdfkl;jklajsdl</p>
+import { API } from './API/Api'
+import BookmarkIcon from '@material-ui/icons/Bookmark';
+function SimilarBooks(props) {
+    const [books, setBooks] = React.useState([])
+    
+    React.useEffect(() =>{
+        function getBooks(){
+            console.log('get similar')
+            API.get('api/similar_books', {
+                params:{
+                    category : props.items.category
+                }
+            })
+            .then(data=>{
+                setBooks(data.data)
+                console.log(data.data)
+            })
+            .catch(err=>{
+                console.log(err)
+            })
+        }
+        getBooks()
+    }, [])
+    
+    const renderBooks = (card, index) =>{
+        if(card.id == props.items.id){
+            return null
+        }
+        return (
+            
+                <div id="cardBlock" key={index}>
+            
+            <div id="itemBlockLink" onClick={() =>{
+                {/*setItemId(card.id)
+                setItemClciked(true)
+                addViews(card.id, card.views)
+                */}
+                }}>
+                <img src={card.image1} alt="cardImage" className="cardImage"/>
+                
             </div>
+            <div id="cardItemInfo">
+                <strong id="cardTitle">{card.title}</strong>
+                <div id="itemPrcBlock">
+                    {card.availability == 'both'?(
+                        <>
+                            <p id="salPrice"><b>Sale Price:</b> &#8377; {card.sale_price}</p>
+                            <p id="borPrice"><b>Borrow Price:</b> &#8377; {card.borrow_price}</p>
+                        </>
+                    ):(
+                        <>
+                            {card.availability == 'sale'?(
+                               <> 
+                                <p id="salPrice"><b>Sale Price:</b> &#8377; {card.sale_price}</p> 
+                                <p id="borPrice"><b>Borrow Price:</b> N/A </p>
+                               </>
+                            ):(
+                                <>
+                                    <p id="salPrice"><b>Sale Price:</b>N/A</p>
+                                    <p id="borPrice"><b>Borrow Price:</b> &#8377; {card.borrow_price}</p>
+                                </>
+                            )}
+                        </>
+                    )}
+                    
+                </div>
+                <div id="cardBlock_button">
+                    {/*<button className="cardButtonSave">Save</button>*/}
+                    {/*{userSession?(
+                        <>
+                        {JSON.stringify(wishList).includes(card.id)?(
+                            <div id="saveButtonCard" onClick={() => validateUser(card.id, 0)}>
+                                <p>Saved</p>
+                                <BookmarkIcon id="bookmarkIcon"/>
+                            </div>
+                        ):( 
+                            <div id="saveButtonCard" onClick={() => validateUser(card.id, 1)}>
+                                <p>Save</p>
+                                <BookmarkIcon id="bookmarkIcon"/>
+                            </div>
+                        )}
+                        </>
+                        ):(*/}
+                        <div id="saveButtonCard" onClick={() => alert('Please Login')}>
+                        <p>Save</p>
+                        <BookmarkIcon id="bookmarkIcon"/>
+                    </div>
+                    {/*)}*/}
+                    
+                    
+                    <button id="cardButtonContact" onClick={() =>{
+                        {/*if(!userSession){
+                            alert('Please Login')
+                            return
+                        }
+                        setCurrentItem(card)
+                        openPopUp()
+                    */}
+                    }}
+                    >Contact Seller</button>
+                    
+                </div>  
+            </div>  
         </div>
+               
+        )
+    }
+    
+
+    return(
+        <div id="similarBooksBlock">
+                <h3>Similar Items</h3>
+                <div id="similarCards" align='center'>
+                    {books.map(renderBooks)}
+            </div></div>
     )
 }
 
