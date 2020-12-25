7c87552b3adb5d2522fde8597f650a17046a8c3b
Bijoy <bijoy10dutta@gmail.com>
2020-12-25 12:11:41 +0530

    Reset Password Page implemented. Send requests completed

---
+++ src/components/pages/ResetPassword.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import EnterID from '../EnterID'
+
+function ResetPassword() {
+    return (
+        <div className="globalBlock">
+            <EnterID/>
+        </div>
+    )
+}
+
+export default ResetPassword
